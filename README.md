c64c35368368ce116defd50da2951fb5c5c69a07
Robert Brisita <986796+rbrisita@users.noreply.github.com>
2024-09-02 12:05:38 -0400

    Adding bash files and README.md

---
+++ README.md
@@ -0,0 +1,34 @@
+# NPU Top
+
+As of now, specifically for the Rockchip RK3588 CPU that contains 3 NPUs @ 2 TOPS each for a total of 6 TOPs.
+
+## Orange Pi 5
+
+### Folders
+
+Information stems from these folders on the Orange Pi 5 (OPi5):
+
+* /proc/rknpu
+* /sys/class/devfreq/fdab0000.npu/
+* /sys/class/devfreq/fdab0000.npu/power
+
+## TODO
+
+Identify NPUs through `dmesg` filtering.
+
+Tera-Operations Per Second (TOPS) default is INT8.
+
+* Google Coral USB Accelerator (4 TOPS)
+* Hailo-8L (13 TOPS)
+* Intel Neural Compute Stick 2 (4 TOPS)
+* Orange Pi AI Stick Lite (5.6 TOPS)
+* Rockchip CPUs
+  * RK1808 (3 TOPS)
+  * RK3399 (3 TOPS)
+  * RK3568 (0.8 TOPS)
+  * RK3588 (6 TOPS)
+* Toybrick TB-RK1808 AI Compute Stick (3 TOPS)
+
+## Resources
+
+* [Bash Simple Curses](https://github.com/metal3d/bashsimplecurses)
---
+++ nputop.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+
+############################################################
+# Read and parse info to create NPU observabiility.
+# Author: Robert Brisita @rbrisita
+# License: Apache-2.0
+############################################################
+
+source ./simple_curses.sh
+
+version=""
+name=""
+
+load_file=""
+
+temp=""
+
+cur_freq=""
+governor=""
+load=""
+
+async=""
+control=""
+runtime_active_kids=""
+runtime_active_time=""
+runtime_enabled=""
+runtime_status=""
+runtime_suspended_time=""
+runtime_usage=""
+
+info() {
+  version=$(cat /proc/rknpu/version)
+  name=$(cat /sys/class/devfreq/fdab0000.npu/name)
+}
+
+load() {
+  load_file=$(cat /proc/rknpu/load | awk -F 'NPU load: ' '{print $2}' | awk '{gsub(",", "|", $0); print}')
+}
+
+temp() {
+  temp=$(sensors | grep -A 3 'npu_thermal-virtual-0' | grep 'temp1' | awk '{print $2}')
+}
+
+details() {
+  cur_freq=$(cat /sys/class/devfreq/fdab0000.npu/cur_freq)
+  governor=$(cat /sys/class/devfreq/fdab0000.npu/governor)
+  load=$(cat /sys/class/devfreq/fdab0000.npu/load)
+}
+
+power() {
+  async=$(cat /sys/class/devfreq/fdab0000.npu/power/async)
+  control=$(cat /sys/class/devfreq/fdab0000.npu/power/control)
+  runtime_active_kids=$(cat /sys/class/devfreq/fdab0000.npu/power/runtime_active_kids)
+  runtime_active_time=$(cat /sys/class/devfreq/fdab0000.npu/power/runtime_active_time)
+  runtime_enabled=$(cat /sys/class/devfreq/fdab0000.npu/power/runtime_enabled)
+  runtime_status=$(cat /sys/class/devfreq/fdab0000.npu/power/runtime_status)
+  runtime_suspended_time=$(cat /sys/class/devfreq/fdab0000.npu/power/runtime_suspended_time)
+  runtime_usage=$(cat /sys/class/devfreq/fdab0000.npu/power/runtime_usage)
+}
+
+main() {
+  # Create gutter
+  window "Rockchip RK3588 CPU" "green" "20%"
+  endwin
+
+  # Move on the next column
+  col_right
+
+  # Create main window
+  window "NPU Top" "green" "50%"
+    append "$version ($name)"
+    addsep
+    append "Load"
+    append_tabbed "$load_file" 3 "|"
+    addsep
+    append "Temperature"
+    append "$temp"
+    addsep
+    append "Details"
+    append_tabbed "Frequency: $cur_freq" 2 ":"
+    append_tabbed "Govenor: $governor" 2 ":"
+    append_tabbed "Load: $load" 2 ":"
+    addsep
+    append "Power"
+    append_tabbed "Async: $async" 2 ":"
+    append_tabbed "Control: $control" 2 ":"
+    append_tabbed "Active Kids: $runtime_active_kids" 2 ":"
+    append_tabbed "Active Time: $runtime_active_time" 2 ":"
+    append_tabbed "Enabled: $runtime_enabled" 2 ":"
+    append_tabbed "Status: $runtime_status" 2 ":"
+    append_tabbed "Suspend Time: $runtime_suspended_time" 2 ":"
+    append_tabbed "Usage: $runtime_usage" 2 ":"
+  endwin
+
+  # Move on the next column
+  col_right
+
+  # Create gutter
+  window "Made with Bash Simple Curses" "green" "20%"
+  endwin
+}
+
+update() {
+  info
+  load
+  temp
+  details
+  power
+}
+
+update
+main_loop
